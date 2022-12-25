fcbdfb08081f1abb27a6ea8dd72bfdc2bb3d5217
Matt Arnold <typeof.developer@gmail.com>
2022-12-25 13:16:07 +0000

    Move registration of commands to top level

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode'
-import { registerCommands } from './commands/registerCommands'
+import { registerCommands } from './registerCommands'
 
 export async function activate (context: vscode.ExtensionContext): Promise<void> {
   const { subscriptions } = context
--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode'
-import { setupCommand } from './setup'
-import { askAboutSelectionCommand } from './askAboutSelection'
-import { testCommand } from './test'
+import { setupCommand } from './commands/setup'
+import { askAboutSelectionCommand } from './commands/askAboutSelection'
+import { testCommand } from './commands/test'
 
 export const registerCommands = (context: vscode.ExtensionContext, subscriptions: vscode.Disposable[]): void => {
   subscriptions.push(setupCommand(context))
