af97a2fdbbbbe9e7508435fc5fd22faa43c3b9ea
Matt Arnold <typeof.developer@gmail.com>
2022-12-25 11:35:47 +0000

    Get selected text from editor

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,11 @@ import { getApiKeyFromUser, getApiKeyFromStorage } from './auth'
 
 export async function activate (context: vscode.ExtensionContext): Promise<void> {
   const { subscriptions } = context
+  const channel = vscode.window.createOutputChannel('GPT Copilot')
 
   subscriptions.push(
     vscode.commands.registerCommand('gpt-copilot.test', () => {
-      const channel = vscode.window.createOutputChannel('GPT Copilot')
+      
       getApiKeyFromStorage(context).then((key) => {
         key !== undefined ? channel.appendLine(key) : channel.appendLine('No API Key')
         channel.show()
@@ -20,6 +21,21 @@ export async function activate (context: vscode.ExtensionContext): Promise<void>
   subscriptions.push(
     vscode.commands.registerCommand('gpt-copilot.setup', () => {
       void getApiKeyFromUser(context)
+    })
+  )
+
+  subscriptions.push(
+    vscode.commands.registerCommand('gpt-copilot.ask_about_selection', () => {
+      const editor = vscode.window.activeTextEditor
+      if (editor) {
+        const selection = editor.selection
+        if (selection && !selection.isEmpty) {
+          const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+          const highlighted = editor.document.getText(selectionRange);
+          channel.appendLine(highlighted)
+          channel.show()
+      }
+    }
     })
   )
 
