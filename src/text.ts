3de0a29e10469943a45780bf881839f4c5f62b88
Matt Arnold <typeof.developer@gmail.com>
2022-12-27 02:44:19 +0000

    Refactor text file

--- src/text.ts
+++ src/text.ts
@@ -2,14 +2,12 @@ import * as vscode from 'vscode'
 
 export const getHighlightedText = (): string => {
   const editor = vscode.window.activeTextEditor
-  let highlighted = ''
   if (editor != null) {
     const selection = editor.selection
     if (!selection.isEmpty) {
       const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character)
-      highlighted = editor.document.getText(selectionRange)
-      return highlighted
+      return editor.document.getText(selectionRange)
     }
   }
-  return highlighted
+  return ''
 }
