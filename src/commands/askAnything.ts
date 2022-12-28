0faff647a781fd29d3737bce53a9b66c6b6491e2
Matt Arnold <typeof.developer@gmail.com>
2022-12-28 18:56:04 +0000

    Fix ask anything command

--- src/commands/askAnything.ts
+++ src/commands/askAnything.ts
@@ -3,8 +3,8 @@ import { writeToConsole, outputChannel, getUserInput } from '../io'
 import { makeRequest } from '../openai'
 
 export const askAnything = (context: vscode.ExtensionContext): vscode.Disposable => {
-  return vscode.commands.registerCommand('gpt-copilot.ask_about_file', async () => {
-      const prompt = await getUserInput('Enter your question', 'What is polymorphism?', 'Invalid question')
+  return vscode.commands.registerCommand('gpt-copilot.ask_anything', async () => {
+    const prompt = await getUserInput('Enter your question', 'What is polymorphism?', 'Invalid question')
     if (prompt !== undefined) {
       const response = await makeRequest(prompt, context.secrets)
       if (response !== undefined) {
